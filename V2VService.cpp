d77b27c4d5866c0bbedefdb189f45390e80af85a
justasAtGU <gusstirju@student.gu.se>
2018-04-26 00:02:46 +0200

    remove terminal interface for v2v protocol

--- V2VService.cpp
+++ V2VService.cpp
@@ -18,36 +18,6 @@ int main(int argc, char **argv) {
       DASH_IP = commandlineArguments["ip"];
       TIME_DIFF = stoi(commandlineArguments["diff"]);
 
-      while (1) {
-        int choice;
-        std::string groupId;
-        std::cout << "Which message would you like to send?" << std::endl;
-        std::cout << "(1) AnnouncePresence" << std::endl;
-        std::cout << "(2) FollowRequest" << std::endl;
-        std::cout << "(3) StopFollow" << std::endl;
-        std::cout << "(#) Nothing, just quit." << std::endl;
-        std::cout << ">> ";
-        std::cin >> choice;
-
-        switch (choice) {
-            case 1: v2vService->announcePresence(); break;
-            case 2: {
-                std::cout << "Which group do you want to follow?" << std::endl;
-                std::cin >> groupId;
-                if (v2vService->presentCars.find(groupId) != v2vService->presentCars.end())
-                    v2vService->followRequest(v2vService->presentCars[groupId]);
-                else std::cout << "Sorry, unable to locate that groups vehicle!" << std::endl;
-                break;
-            }
-            case 3: {
-                std::cout << "Which group do you want to stop follow?" << std::endl;
-                std::cin >> groupId;
-                if (v2vService->presentCars.find(groupId) != v2vService->presentCars.end())
-                    v2vService->stopFollow(v2vService->presentCars[groupId]);
-                else std::cout << "Sorry, unable to locate that groups vehicle!" << std::endl;
-                break;
-            } default: exit(0);
-        }
       internal =
       std::make_shared<cluon::OD4Session>(INTERNAL_CHANNEL,
         [](cluon::data::Envelope &&envelope) noexcept {
@@ -82,7 +52,7 @@ int main(int argc, char **argv) {
 
       }
     }
-  }
+  
 
 
 /**
